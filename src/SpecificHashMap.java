442794b8b7b11edb53961572aea69123f4f5c57e
Nate Langenkamp <langstore8@gmail.com>
2015-05-06 22:21:32 -0500

    Specific Hash Map
    TEST FOR ERRORS BECAUSE YOUR SCREEN IS BIG AND PRETTY AND ALSO NEEDS
    COMMENTING BUT THE DISPLAY METHOD IF WORKING CAN BE COPIED TO
    GENERALHASHMAP NOW

--- src/SpecificHashMap.java
+++ src/SpecificHashMap.java
@@ -7,7 +7,7 @@ public class SpecificHashMap extends MyHashMap {
 
 	@SuppressWarnings("unchecked")
 	public SpecificHashMap() {
-		this.length = 143;
+		this.length = 53;
 		this.buckets = (LinkedList<String>[]) new LinkedList[length];
 		for(int i=0; i<length;i++){
 			this.buckets[i] = new LinkedList<String>();
@@ -16,21 +16,178 @@ public class SpecificHashMap extends MyHashMap {
 
 	@Override
 	protected int hash(String token) {
-/*
-		int totAscii = (int)token.charAt(0);
-		int hashIndex;
-		int len = token.length();
-
-		for(int i = 1; i < len; i++){//generating an index
-			totAscii += i*(int)token.charAt(i)*2;
+		int size = token.length();
+		if(size == 2){
+			if(token.equals("do")){
+				return 0;
+			}
+			else if(token.equals("if")){
+				return 1;
+			}
 		}
-		hashIndex = totAscii;
-
-		while(hashIndex >= this.length){
-			//System.out.println(hashIndex);
-			hashIndex /= 10;
+		else if(size ==3){
+			if(token.equals("for")){
+				return 2;
+			}
+			else if(token.equals("new")){
+				return 3;
+			}
+			else if(token.equals("int")){
+				return 4;
+			}
+			else if(token.equals("try")){
+				return 5;
+			}
 		}
-*/
+		else if(size ==4){
+			if(token.equals("this")){
+				return 6;
+			}
+			else if(token.equals("byte")){
+				return 7;
+			}
+			else if(token.equals("else")){
+				return 8;
+			}
+			else if(token.equals("case")){
+				return 9;
+			}
+			else if(token.equals("enum")){
+				return 10;
+			}
+			else if(token.equals("char")){
+				return 11;
+			}
+			else if(token.equals("void")){
+				return 12;
+			}
+			else if(token.equals("long")){
+				return 13;
+			}
+		}
+		else if(size ==5){
+			if(token.equals("goto*")){
+				return 14;
+			}
+			else if(token.equals("break")){
+				return 15;
+			}
+			else if(token.equals("throw")){
+				return 16;
+			}
+			else if(token.equals("short")){
+				return 17;
+			}
+			else if(token.equals("catch")){
+				return 18;
+			}
+			else if(token.equals("final")){
+				return 19;
+			}
+			else if(token.equals("class")){
+				return 20;
+			}
+			else if(token.equals("const")){
+				return 21;
+			}
+			else if(token.equals("float")){
+				return 22;
+			}
+			else if(token.equals("super")){
+				return 23;
+			}
+			else if(token.equals("while")){
+				return 24;
+			}
+		}
+		else if(size ==6){
+			if(token.equals("switch")){
+				return 25;
+			}
+			else if(token.equals("assert")){
+				return 26;
+			}
+			else if(token.equals("double")){
+				return 27;
+			}
+			else if(token.equals("import")){
+				return 28;
+			}
+			else if(token.equals("public")){
+				return 29;
+			}
+			else if(token.equals("throws")){
+				return 30;
+			}
+			else if(token.equals("return")){
+				return 31;
+			}
+			else if(token.equals("static")){
+				return 32;
+			}
+			else if(token.equals("native")){
+				return 33;
+			}
+		}
+		else if(size ==7){
+			if(token.equals("default")){
+				return 34;
+			}
+			else if(token.equals("package")){
+				return 35;
+			}
+			else if(token.equals("boolean")){
+				return 36;
+			}
+			else if(token.equals("private")){
+				return 37;
+			}
+			else if(token.equals("extends")){
+				return 38;
+			}
+			else if(token.equals("finally")){
+				return 39;
+			}
+		}
+		else if(size ==8){
+			if(token.equals("abstract")){
+				return 40;
+			}
+			else if(token.equals("continue")){
+				return 41;
+			}
+			else if(token.equals("strictfp")){
+				return 42;
+			}
+			else if(token.equals("volatile")){
+				return 43;
+			}
+		}
+		else if(size ==9){
+			if(token.equals("transient")){
+				return 44;
+			}
+			else if(token.equals("protected")){
+				return 45;
+			}
+			else if(token.equals("interface")){
+				return 46;
+			}
+		}
+		else if(size ==10){
+			if(token.equals("implements")){
+				return 47;
+			}
+			else if(token.equals("instanceof")){
+				return 48;
+			}
+		}
+		else if(size ==11){
+			return 49;
+		}
+
+
+
 		return 0;//hashIndex;
 		
 	}
@@ -53,8 +210,12 @@ public class SpecificHashMap extends MyHashMap {
 	@Override
 	public void display() {
 		
-		// TODO: IMPLEMENT DISPLAY METHOD TO SHOW CONTENTS OF ALL BUCKETS
-		
+		for(int i = 0; i < length; i++){
+			System.out.println(buckets[i] + ": ");
+			for(int q = 0; q < buckets[i].size(); q++;){
+				System.out.println("   " + buckets[i].get(q));
+			}
+		}
 	}
 	
 }
